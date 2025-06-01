426da15c50e6cc08213bcebdea8eb30dad7a20f7
Samuel Simplicio <samuelsimplicio5@gmail.com>
2025-06-01 17:55:45 -0300

    Ad ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 pub mod apis;
+pub mod interface;
 
 use crate::apis::{jira,github};
 use apis::openai;
+use crossterm::event;
+use interface::app::App;
+use ratatui::{DefaultTerminal, Frame};
 use reqwest::Client;
 use serde::Deserialize;
 use anyhow::Result;
@@ -14,8 +18,22 @@ struct Variables {
     openai: openai::UserData,
 }
 
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if matches!(event::read()?, event::Event::Key(_)) {
+            break Ok(());
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    /**
     let issue = github::Issue {
         title: "Rust test".to_string(),
         body: "All cool.".to_string(),
@@ -27,13 +45,11 @@ async fn main() -> Result<()> {
     let vars: Variables = serde_json::from_str(&contents)?;
     let client = Client::new();
 
-    /**
     let res = github::create_issue(&vars.github, &client, "jira-bot", issue).await;
     println!("{:?}", res);
 
     let res = jira::create_issue(&vars.jira, &client, "Test issue", "test description").await;
     println!("{:?}", res);
-    */
 
     let res = openai::create_story(&vars.openai, &client, "Crie uma story de usuário para: Tela de usuários (gerente): adicionar usuário").await;
     match res {
@@ -43,5 +59,7 @@ async fn main() -> Result<()> {
             println!("{:?}", e)
     };
 
+    Ok(())
+    */
     Ok(())
 }
